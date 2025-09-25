a206d4399ae6a4fa829b1a691c307341f4b7cd62
Marko <frontenddevtipstricks@gmail.com>
2025-09-25 20:58:32 +0200

    react forms

---
+++ 04_React_Forms/src/App.jsx
@@ -0,0 +1,19 @@
+import FormContainer from './components/FormContainer'
+import RegistrationForm from './components/RegistrationForm'
+import LoginForm from './components/LoginForm'
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      {/* <FormContainer /> */}
+      <RegistrationForm />
+      <hr/>
+      <LoginForm />
+    </>
+  )
+}
+
+export default App
